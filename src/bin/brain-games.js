304d99facc5b55d5372b73f3c863f53013e4347c
DeltaSPb <oipts3012@gmail.com>
2018-05-29 11:59:07 +0300

    fix eslint errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greeting } from '..';
+import greeting from '..';
 
 console.log('Welcome to the Brain Games!');
 greeting();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-const player = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${player}!`);
+export default () => {
+  const player = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${player}!`);
 };
