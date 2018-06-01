689f799efda1b53ce336a422630976e95130ed93
DeltaSPb <oipts3012@gmail.com>
2018-06-01 14:01:01 +0300

    add balance game

---
+++ src/games/balance-game.js
@@ -0,0 +1,27 @@
+import playGame from '..';
+import { getRandomNum } from '../utils';
+
+const rule = 'Balance the given number.';
+
+const getBalance = (num) => {
+  let digits = String(num).split('').map(Number).sort();
+  const index = digits.length - 1;
+
+  while (digits[index] - digits[0] > 1) {
+    digits[0] += 1;
+    digits[index] -= 1;
+    digits = digits.sort();
+  }
+
+  return Number(digits.join(''));
+};
+
+const quesionsForBalance = () => {
+  const number = getRandomNum(100, 500);
+  const question = `${number}`;
+  const answer = String(getBalance(number));
+
+  return { question, answer };
+};
+
+export default () => playGame(quesionsForBalance, rule);
